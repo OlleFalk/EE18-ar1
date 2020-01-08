4fcfd5dadc75628c3d55dd705881d1ea7030c123
Olle Falk <olle.falk@learnet.se>
2020-01-08 13:32:32 +0100

    repetera javascript , input

---
+++ javascript/6-labbar/spel/spel.js
@@ -0,0 +1,21 @@
+/* 
+    * Hasse & Tage filmar "släpp fångarne loss, det är vår!" på långholmens centralfängelse. fängelset läggs ner samma år. 1975
+
+    * I Frankrike föds organisationen "Trädgårdstomtarnas befrielsefront." 1996
+
+    * Yassir Arafat blir ledare för PLO. 1969
+
+    * Leanardo da Vinci avslutar verket Mona Lisa. 1503
+
+    * Fucking Åmol är detta års bästa film enligt Guldbaggejuryn. 1998
+
+    * Med slagord som "den vite spelar boll, den svarte ingen roll" vill demonstranter stoppa en tennismatch mellan sverige och rhodesia i båstad. 1968
+
+    * Sara Lidman åtalas i Sydafrika för brott mot raslagarna. 1961
+
+    * Salman Rushdie ger ut sin bok "satansverserna" och får en fatwa på halsen. Ayatolla Khomeyni tycker alla ska dö pga sina smädelser. 1988
+
+    * Thomas Edison visar upp sin första funktionella glödlampa. 1879
+
+    * Svenskt förbud att blanda kadavermjöl i djurfoder. 1986
+*/
